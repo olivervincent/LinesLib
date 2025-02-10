13c580bdcb6b6e2fd4a1ecab4b3cefcc446e0ba5
Oliver Vincent <oliverlund3@gmail.com>
2025-02-10 11:14:32 +0100

    Tests with datarows

--- LinesLib.Tests/LineSegmentTest.cs
+++ LinesLib.Tests/LineSegmentTest.cs
@@ -1,172 +1,120 @@
 using System;
-using System.Collections.Generic;
-using JetBrains.Annotations;
-using LinesLib;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-namespace LinesLib.Tests;
-
-[TestClass]
-[TestSubject(typeof(LineSegment))]
-public class LineSegmentTest
-{
-
-    [TestMethod]
-    public void ConstructorTest()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        Assert.AreEqual(1, lineSegment.start);
-        Assert.AreEqual(2, lineSegment.end);
-        LineSegment lineSegment2 = new LineSegment(4, 4);
-        Assert.ThrowsException<ArgumentException>(() => new LineSegment(2, 1));
-    }
+    using System.Collections.Generic;
+    using JetBrains.Annotations;
+    using LinesLib;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
     
-    [TestMethod]
-    public void ToStringTest()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        Assert.AreEqual("(1, 2)", lineSegment.ToString());
-    }
+    namespace LinesLib.Tests;
     
-    [TestMethod]
-    public void ContainsTest()
+    [TestClass]
+    [TestSubject(typeof(LineSegment))]
+    public class LineSegmentTest
     {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        Assert.IsTrue(lineSegment.Contains(1));
-        Assert.IsTrue(lineSegment.Contains(2));
-        Assert.IsFalse(lineSegment.Contains(0));
-        Assert.IsFalse(lineSegment.Contains(3));
-        LineSegment lineSegment2 = new LineSegment(1, 2);
-        Assert.IsTrue(lineSegment.Contains(lineSegment2));
-        LineSegment lineSegment3 = new LineSegment(1, 3);
-        Assert.IsFalse(lineSegment.Contains(lineSegment3));
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, 2)]
+        [DataRow(4, 4, 4, 4)]
+        public void ConstructorTest(int start, int end, int expectedStart, int expectedEnd)
+        {
+            LineSegment lineSegment = new LineSegment(start, end);
+            Assert.AreEqual(expectedStart, lineSegment.start);
+            Assert.AreEqual(expectedEnd, lineSegment.end);
+        }
     
-    [TestMethod]
-    public void ContainsTest2()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(1, 2);
-        Assert.IsTrue(lineSegment.Contains(lineSegment2));
-        LineSegment lineSegment3 = new LineSegment(1, 3);
-        Assert.IsFalse(lineSegment.Contains(lineSegment3));
-    }
+        [TestMethod]
+        [DataRow(1, 2, "(1, 2)")]
+        public void ToStringTest(int start, int end, string expected)
+        {
+            LineSegment lineSegment = new LineSegment(start, end);
+            Assert.AreEqual(expected, lineSegment.ToString());
+        }
     
-    [TestMethod]
-    public void EqualsTest()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(1, 2);
-        Assert.AreEqual(lineSegment, lineSegment2);
-        
-        LineSegment lineSegment3 = new LineSegment(1, 3);
-        Assert.AreNotEqual(lineSegment, lineSegment3);
-        
-        Assert.AreNotEqual(lineSegment, null);
-        
-        Assert.AreNotEqual<object>(lineSegment, "not a line segment");
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, true)]
+        [DataRow(1, 2, 2, true)]
+        [DataRow(1, 2, 0, false)]
+        [DataRow(1, 2, 3, false)]
+        public void ContainsTest(int start, int end, int value, bool expected)
+        {
+            LineSegment lineSegment = new LineSegment(start, end);
+            Assert.AreEqual(expected, lineSegment.Contains(value));
+        }
     
-    [TestMethod]
-    public void GetHashCodeTest()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(1, 2);
-        Assert.AreEqual(lineSegment.GetHashCode(), lineSegment2.GetHashCode());
-        LineSegment lineSegment3 = new LineSegment(1, 3);
-        Assert.AreNotEqual(lineSegment.GetHashCode(), lineSegment3.GetHashCode());
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, 2, true)]
+        [DataRow(1, 2, 1, 3, false)]
+        public void ContainsTest2(int start1, int end1, int start2, int end2, bool expected)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            Assert.AreEqual(expected, lineSegment1.Contains(lineSegment2));
+        }
     
-    [TestMethod]
-    public void IntersectionTest()
-    {
-        LineSegment lineSegment = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(1, 3);
-        LineSegment lineSegment3 = new LineSegment(2, 3);
-        LineSegment lineSegment4 = new LineSegment(3, 4);
-        Assert.AreEqual(new LineSegment(1, 2), lineSegment.Intersection(lineSegment2));
-        Assert.AreEqual(new LineSegment(2, 2), lineSegment.Intersection(lineSegment3));
-        Assert.IsNull(lineSegment.Intersection(lineSegment4));
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, 2, true)]
+        [DataRow(1, 2, 1, 3, false)]
+        public void EqualsTest(int start1, int end1, int start2, int end2, bool expected)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            Assert.AreEqual(expected, lineSegment1.Equals(lineSegment2));
+        }
     
-    [TestMethod]
-    public void UnionTest_NonOverlappingSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(3, 4);
-        List<LineSegment> result = lineSegment1.Union(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 2), new LineSegment(3, 4) }, result);
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, 2, true)]
+        [DataRow(1, 2, 1, 3, false)]
+        public void GetHashCodeTest(int start1, int end1, int start2, int end2, bool expected)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            Assert.AreEqual(expected, lineSegment1.GetHashCode() == lineSegment2.GetHashCode());
+        }
     
-    [TestMethod]
-    public void UnionTest_OverlappingSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 3);
-        LineSegment lineSegment2 = new LineSegment(2, 4);
-        List<LineSegment> result = lineSegment1.Union(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 4) }, result);
-    }
+        [TestMethod]
+        [DataRow(1, 2, 1, 3, 1, 2)]
+        [DataRow(1, 2, 2, 3, 2, 2)]
+        [DataRow(1, 2, 3, 4, null, null)]
+        public void IntersectionTest(int start1, int end1, int start2, int end2, int? expectedStart, int? expectedEnd)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            LineSegment expected = expectedStart.HasValue ? new LineSegment(expectedStart.Value, expectedEnd.Value) : null;
+            Assert.AreEqual(expected, lineSegment1.Intersection(lineSegment2));
+        }
     
-    [TestMethod]
-    public void UnionTest_AdjacentSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(2, 3);
-        List<LineSegment> result = lineSegment1.Union(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 3) }, result);
-    }
+        [TestMethod]
+        [DataRow(1, 2, 3, 4, new[] { 1, 2, 3, 4 })]
+        [DataRow(1, 3, 2, 4, new[] { 1, 4 })]
+        [DataRow(1, 2, 2, 3, new[] { 1, 3 })]
+        [DataRow(1, 4, 2, 3, new[] { 1, 4 })]
+        public void UnionTest(int start1, int end1, int start2, int end2, int[] expected)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            List<LineSegment> result = lineSegment1.Union(lineSegment2);
+            List<LineSegment> expectedList = new List<LineSegment>();
+            for (int i = 0; i < expected.Length; i += 2)
+            {
+                expectedList.Add(new LineSegment(expected[i], expected[i + 1]));
+            }
+            CollectionAssert.AreEqual(expectedList, result);
+        }
     
-    [TestMethod]
-    public void UnionTest_ContainedSegment()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 4);
-        LineSegment lineSegment2 = new LineSegment(2, 3);
-        List<LineSegment> result = lineSegment1.Union(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 4) }, result);
+        [TestMethod]
+        [DataRow(1, 4, 2, 3, new[] { 1, 1, 4, 4 })]
+        [DataRow(2, 3, 1, 4, new int[] { })]
+        [DataRow(1, 3, 2, 4, new[] { 1, 1 })]
+        [DataRow(1, 2, 3, 4, new[] { 1, 2 })]
+        [DataRow(1, 2, 2, 3, new[] { 1, 1 })]
+        public void MinusTest(int start1, int end1, int start2, int end2, int[] expected)
+        {
+            LineSegment lineSegment1 = new LineSegment(start1, end1);
+            LineSegment lineSegment2 = new LineSegment(start2, end2);
+            List<LineSegment> result = lineSegment1.Minus(lineSegment2);
+            List<LineSegment> expectedList = new List<LineSegment>();
+            for (int i = 0; i < expected.Length; i += 2)
+            {
+                expectedList.Add(new LineSegment(expected[i], expected[i + 1]));
+            }
+            CollectionAssert.AreEqual(expectedList, result);
+        }
     }
-    
-    [TestMethod]
-    public void MinusTest()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 4);
-        LineSegment lineSegment2 = new LineSegment(2, 3);
-        List<LineSegment> result = lineSegment1.Minus(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 1), new LineSegment(4, 4) }, result);
-    }
-    
-    [TestMethod]
-    public void MinusTest_ContainedSegment()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 4);
-        LineSegment lineSegment2 = new LineSegment(2, 3);
-        List<LineSegment> result = lineSegment2.Minus(lineSegment1);
-        CollectionAssert.AreEqual(new List<LineSegment>(), result);
-    }
-    
-    [TestMethod]
-    public void MinusTest_OverlappingSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 3);
-        LineSegment lineSegment2 = new LineSegment(2, 4);
-        List<LineSegment> result = lineSegment1.Minus(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 1) }, result);
-    }
-    
-    [TestMethod]
-    public void MinusTest_NonOverlappingSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(3, 4);
-        List<LineSegment> result = lineSegment1.Minus(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 2) }, result);
-    }
-    
-    [TestMethod]
-    public void MinusTest_AdjacentSegments()
-    {
-        LineSegment lineSegment1 = new LineSegment(1, 2);
-        LineSegment lineSegment2 = new LineSegment(2, 3);
-        List<LineSegment> result = lineSegment1.Minus(lineSegment2);
-        CollectionAssert.AreEqual(new List<LineSegment> { new LineSegment(1, 1) }, result);
-    }
-}
